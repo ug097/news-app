e9db5649dc543a5c084dfefbf83386b4d7e91de6
097yuji <g621308@gmail.com>
2021-02-15 01:50:15 +0900

    APIからニュースを取得、羅列表示

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
+    const [articles, setArticles] = useState([]);
+    //   stateの変数名 ←に値セットする関数を宣言　articlesの初期値を指定（ここでは空）
+
+    useEffect(() => {
+        // 第二引数に空の配列[]を渡した場合、マウント時に第一引数の関数が実行される
+        fetchArticles();
+    }, []);
+
+    const fetchArticles = async () => {
+        try {
+            const response = await axios.get(URL);
+            setArticles(response.data.articles); // 引数のデータがstate変数のarticlesに入る
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
-        <View style={styles.container}>
-            <View style={styles.itemContainer}>
-                <View style={styles.leftContainer}>
-                    <Image
-                        style={{ width: 100, height: 100 }}
-                        source={{ uri: 'https://picsum.photos/id/10/200/200' }}
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles} // FlatListで表示したいデータの配列を入れる
+                renderItem={(
+                    { item } // itemはarticles配列の１項目
+                ) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
                     />
-                </View>
-                <View style={styles.rightContainer}>
-                    <Text numberOfLines={3} style={styles.text}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation
-                    </Text>
-                    <Text style={styles.subText}>ReactNews</Text>
-                </View>
-            </View>
-        </View>
+                )}
+                keyExtractor={(item, index) => index.toString()} // 配列内各項目にユニークなキーの指定が必要
+            />
+        </SafeAreaView>
     );
 }
 
@@ -29,29 +46,5 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    itemContainer: {
-        height: 100,
-        width: '100%',
-        borderColor: 'gray',
-        borderWidth: 1,
-        flexDirection: 'row',
-    },
-    leftContainer: {
-        width: 100,
-    },
-    rightContainer: {
-        flex: 1,
-        padding: 10,
-        justifyContent: 'space-between',
-    },
-    text: {
-        fontSize: 16,
-    },
-    subText: {
-        fontSize: 12,
-        color: 'gray',
     },
 });
