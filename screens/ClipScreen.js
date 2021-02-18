0305780310f6a9914af0c51d1adb40fb54a694d8
097yuji <g621308@gmail.com>
2021-02-19 00:59:32 +0900

    Clip画面にClipリストを表示する

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { StyleSheet, SafeAreaView, Text, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
 const styles = StyleSheet.create({
     container: {
@@ -8,10 +10,30 @@ const styles = StyleSheet.create({
     },
 });
 
-export default ClipScreen = () => {
+export default ClipScreen = ({ navigation }) => {
+    const user = useSelector(state => state.user);
+    const { clips } = user;
+
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Clip Screen</Text>
+            <FlatList
+                data={clips} // FlatListで表示したいデータの配列を入れる
+                renderItem={(
+                    { item } // itemはclips配列のうちの１要素
+                ) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
+                        onPress={() =>
+                            navigation.navigate('Article', {
+                                article: item, // ここで名付けたarticleというキー名を遷移先で使用し、中身のitemを展開する
+                            })
+                        }
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()} // 配列内各項目にユニークなキーの指定が必要
+            />
         </SafeAreaView>
     );
 };
