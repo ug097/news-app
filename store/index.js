cf34fcd4829b4ac0c96492f8e6a1416e2ae63d06
097yuji <g621308@gmail.com>
2021-02-20 02:28:56 +0900

    クリップ情報を端末に保存する

--- store/index.js
+++ store/index.js
@@ -1,12 +1,22 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { persistReducer, persistStore } from 'redux-persist';
 import userReducer from './reducers/user';
+import { AsyncStorage } from 'react-native';
 
 const rootReducer = combineReducers({
     user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whiteList: ['user'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
